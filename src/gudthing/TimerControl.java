3d7d85c66822de66b86fca1a1e5f5fdc47684eff
Benjamin Scabbia <benscabbia@gmail.com>
2015-09-07 18:47:05 +0100

    -added a TimerControl class -implemented a timer which will show time until system shutdown.

---
+++ src/gudthing/TimerControl.java
@@ -0,0 +1,63 @@
+package gudthing;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Ben on 07/09/2015.
+ */
+public class TimerControl {
+    Timer t;
+    JLabel timeLabel;
+    int time;
+    int sec, min, hour;
+
+    /*Constructor which points class to label*/
+    public TimerControl(JLabel timeLabel) {
+        this.timeLabel = timeLabel;
+    }
+
+    //beed to fix HERE
+    public void startTimer(int startTime) {
+        this.time = startTime;
+        formatTime(time);
+
+        t = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                decrementTime();
+                timeLabel.setText(formatTime(time));
+                //System.out.println("The time: " + time);
+
+            }
+        });
+        t.start();
+        timeLabel.setText(formatTime(time));
+    }
+
+    public void stopTimer() {
+        t.stop();
+    }
+
+    public String formatTime(long millis) {
+        /*sec = (int) (milliseconds / 1000) % 60 ;
+        min = (int) ((milliseconds / (1000*60)) % 60);
+        hour = (int) ((milliseconds / (1000*60*60)) % 24);*/
+        //System.out.println(millis);
+        return String.format("%02d:%02d:%02d",
+                TimeUnit.SECONDS.toHours(millis),
+                TimeUnit.SECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.SECONDS.toHours(millis)),
+                TimeUnit.SECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(millis)));
+
+    }
+
+    private void decrementTime() {
+        //System.out.println("before de " + time);
+        time -= 1;
+        //System.out.println("after de " + time);
+    }
+
+
+}
